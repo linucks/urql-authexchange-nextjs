174114b8822ce8f544bb56303262dff34ef3a0a8
Jens Thomas <linucks42@gmail.com>
2025-01-07 21:59:42 +0000

    Import exchange code & update for next

---
+++ app/authstore.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+const TOKEN_KEY = "token";
+const REFRESH_TOKEN_KEY = "refresh_token";
+
+export const saveAuthData = async ({ token, refreshToken }) => {
+  (await cookies()).set({
+    name: TOKEN_KEY,
+    value: token,
+  });
+  (await cookies()).set({
+    name: REFRESH_TOKEN_KEY,
+    value: refreshToken,
+  });
+};
+
+export const getToken = async () => {
+  return (await cookies()).get(TOKEN_KEY)?.value;
+};
+
+export const getRefreshToken = async () => {
+  return (await cookies()).get(REFRESH_TOKEN_KEY)?.value;
+};
+
+export const clearStorage = async () => {
+  (await cookies()).delete(TOKEN_KEY);
+  (await cookies()).delete(REFRESH_TOKEN_KEY);
+  localStorage.clear();
+};
